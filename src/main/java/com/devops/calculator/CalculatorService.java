5ad2661025263610591d4c052ee3cfb0f0f0e3ff
Mohamed Taher <mrmedtb@gmail.com>
2025-06-20 02:29:21 +0200

    initial commit

---
+++ src/main/java/com/devops/calculator/CalculatorService.java
@@ -0,0 +1,54 @@
+package com.devops.calculator;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.Date;
+
+@Path("/calculator")
+public class CalculatorService {
+
+    @GET
+    @Path("ping")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String ping() {
+        return "Welcome to Java Maven Calculator Web App!!!\n" + new Date().toString();
+    }
+
+    public Object checkNull(Object obj){
+        if(obj != null){
+            return  obj;
+        }
+        return null;
+    }
+
+    @GET
+    @Path("add")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CalculatorResponse Add(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new CalculatorResponse(x, y, x + y);
+    }
+
+    @GET
+    @Path("sub")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CalculatorResponse Sub(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new CalculatorResponse(x, y, x - y);
+    }
+
+    @GET
+    @Path("mul")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CalculatorResponse Mul(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new CalculatorResponse(x, y, x * y);
+    }
+
+    @GET
+    @Path("div")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CalculatorResponse Div(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new CalculatorResponse(x, y, x / y);
+    }
+}
